42432c487db1e4ff2f883a37535ffa23b5d91cf2
黃郁凱 <ken40471@gmail.com>
2021-06-24 20:00:21 +0800

    final

---
+++ 410716160_0624/Controllers/ExamController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _410716160_0624.Controllers
+{
+    public class ExamController : Controller
+    {
+        // GET: Exam
+        public ActionResult About()
+        {
+            return View();
+        }
+    }
+}
